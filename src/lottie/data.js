12c16154d0b0dbe77a9dc7bfb21f1198aa147ea1
zayyid123 <mochamadzayyid@gmail.com>
2024-04-13 05:45:38 +0700

    add new animation

--- src/lottie/data.js
+++ src/lottie/data.js
@@ -1,8 +1,12 @@
 import * as animationData1 from "@/lottie/key_animation.json";
+import * as animationData2 from "@/lottie/ship_animation.json";
 
 const DataANIMATION = [
   {
     animation: animationData1,
+  },
+  {
+    animation: animationData2,
   },
 ];
 
